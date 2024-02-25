71cef3fce69373b1af88671dac9554f41ae38722
Aleksei <leffalexx@gmail.com>
2024-02-25 15:01:03 +0400

    made rotors separate

--- encoder.py
+++ encoder.py
@@ -1,62 +1,11 @@
+import json
 
-first_rotor = {
-    'a': 'z',
-    'b': 'y', 
-    'c': 'x',
-    'd': 'w',
-    'e': 'v',
-    'f': 'u',
-    'g': 't',
-    'h': 's',
-    'i': 'r',
-    'j': 'q',
-    'k': 'p',
-    'l': 'o',
-    'm': 'n',
-    'n': 'm',
-    'o': 'l',
-    'p': 'k',
-    'q': 'j',
-    'r': 'i',
-    's': 'h',
-    't': 'g',
-    'u': 'f',
-    'v': 'e',
-    'w': 'd',
-    'x': 'c',
-    'y': 'b',
-    'z': 'a',
-    ' ': ' '
-    }
-second_rotor = {
-  'a': 'm',
-  'b': 'g',
-  'c': 'l',
-  'd': 'p',
-  'e': 'o', 
-  'f': 'j',
-  'g': 'u',
-  'h': 'q',
-  'i': 'k',
-  'j': 'c',
-  'k': 'n',
-  'l': 'y',
-  'm': 'z',
-  'n': 'h',
-  'o': 'w',
-  'p': 'f',
-  'q': 'a',
-  'r': 'x',
-  's': 'e',
-  't': 'd',
-  'u': 'r',
-  'v': 'b',
-  'w': 'i',
-  'x': 't',
-  'y': 'v',
-  'z': 's',
-  ' ': ' '
-}
+filenames = ['first_rotor.json', 'second_rotor.json']
+rotors = dict(map(lambda filename: (filename.split('.')[0], json.load(open(filename))), filenames))
+
+first_rotor = rotors['first_rotor']
+second_rotor = rotors['second_rotor']
+
 
 def shift_rotor(rotor_state):
     new_position = {}
@@ -85,4 +34,6 @@ def encode_string(text, first_rotor, second_rotor):
             shift_rotor(second_rotor)
     return encoded
         
-print(encode_string('hello world and all the loveley people out there this is a prototype enigmna code with just two rotors and no reflector or plugpanel', first_rotor, second_rotor))
+print(encode_string('artamun is a nice dog and artamun feasts on cats', first_rotor, second_rotor))
+
+#print(rotors)
